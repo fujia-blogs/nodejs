e6ce5dc6a9cdf55ab065190a8f4a14b51ba0c0ad
fujia <fujia@lizhi.co>
2022-08-15 07:45:40 +0800

    docs: update materials

--- examples/advance-js/demos/checkType.js
+++ examples/advance-js/demos/checkType.js
@@ -1,3 +1,5 @@
+const _ = console.log;
+
 // console.log(typeof null);
 
 // console.log(typeof NaN);
@@ -16,9 +18,22 @@ String.prototype.constructor = function fun() {
   return {};
 };
 
-console.log('str'.constructor);
+// console.log('str'.constructor);
 
 // underscore
 function isUndefined(obj) {
   return obj === void 0;
 }
+
+const arr = [NaN];
+
+// _(arr.indexOf(NaN));
+// _(arr.includes(NaN));
+
+function format_with_regex(num) {
+  const reg = /\d{1,3}(?=(\d{3})+$)/g;
+
+  return (num + '').replace(reg, '$&,');
+}
+
+_(format_with_regex(987654321));
---
+++ examples/advance-js/demos/obj-basic.js
@@ -0,0 +1,21 @@
+const _ = console.log;
+
+const obj = {};
+
+obj.p1 = 'str1';
+obj.p6 = 'str6';
+obj.p2 = 'str2';
+
+obj[1] = 'num1';
+obj[6] = 'num6';
+obj[2] = 'num2';
+
+// for (let p in obj) {
+//   _('property: ', obj[p]);
+// }
+
+// let arr = [0, 1, 2, 3, 4, 5];
+
+// _(arr['1']);
+// _(arr['2']);
+// _(arr['3']);
